fa422678e4d6c9c41f7ae92dcbf4744cc1e81090
Daniel Lundin <daniel.lundin@scilifelab.se>
2015-02-13 13:06:35 +0100

    Adding contact page. CSS stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
 
+  get 'static_pages/contact'
+  get 'static_pages/home'
   get 'static_pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,4 +12,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select 'title', 'pfitmap::help'
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "pfitmap::contact"
+  end
 end
