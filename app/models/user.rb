6110377b67426fb99e27f276571123b71a3057af
Daniel Lundin <daniel.lundin@scilifelab.se>
2015-02-16 17:02:50 +0100

    Working on logging in and out

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,11 @@ class User < ActiveRecord::Base
   def set_default_role
     self.role ||= :user
   end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
