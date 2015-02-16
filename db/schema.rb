99ae5aea949ff031711f9f6cf4a4338f6aec90a6
Daniel Lundin <daniel.lundin@scilifelab.se>
2015-02-16 23:23:51 +0100

    Cookies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216131205) do
+ActiveRecord::Schema.define(version: 20150216201413) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255, null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150216131205) do
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
     t.integer  "role",            limit: 4
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
