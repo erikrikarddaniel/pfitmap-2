020c46653ed41833357bcabb9f7d0355ab8a1183
Daniel Lundin <daniel.lundin@scilifelab.se>
2015-02-16 14:28:30 +0100

    Adding admin user to seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216113723) do
+ActiveRecord::Schema.define(version: 20150216131205) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255, null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150216113723) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
+    t.integer  "role",            limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
